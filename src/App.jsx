603915fa71e1447cfaedd1d4b791b3a415b79e84
ahad1033 <a.ahad2232@gmail.com>
2023-08-14 05:32:31 +0600

    navBar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <NavBar />
+    </div>
   )
 }
 
